6b64104e85d88199b7d5490189798c63189730df
Shourya Gupta <119586614+Fireseeker23@users.noreply.github.com>
2024-10-23 16:46:56 +0530

    Update 2.py
    converted encoders to functions

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-
-test_data = 'test.csv'
+#data
+test_data = 'data/test.csv'
 home_data = pd.read_csv(test_data)
 
 
@@ -23,38 +23,48 @@ for col in home_data.columns:
     if home_data[col].dtype == 'object':
         cat_columns.append(col)
 
-
-
-
 #Ordinal encoder
-print(home_data[cat_columns].head(10))
+def OE(cat_columns, home_data):
+    i = 0
+    for col in cat_columns:
+        unique_elements = home_data[cat_columns[i]].unique()
+        j = 0 
+        while j < len(unique_elements):
+            k = 0
+            while k < len(home_data[cat_columns[i]]):
+                if home_data.loc[k,cat_columns[i]] == unique_elements[j]:
+                    home_data.loc[k,cat_columns[i]] = j + 1
+                k = k + 1
+            j = j + 1
+        i = i + 1
 
-i = 0
-for col in cat_columns:
-    unique_elements = home_data[cat_columns[i]].unique()
-    j = 0 
-    while j < len(unique_elements):
-        k = 0
-        while k < len(home_data[cat_columns[i]]):
-            if home_data.loc[k,cat_columns[i]] == unique_elements[j]:
-                home_data.loc[k,cat_columns[i]] = j + 1
-            k = k + 1
-        j = j + 1
-    i = i + 1
-
-print(home_data[cat_columns].head(10))
 
 
 #One hot encoder
-new_columns = {}
-for col in cat_columns:
-    unique_elements = home_data[col].unique()
-    for unique_value in unique_elements:
-        new_columns[f"{col}_{unique_value}"] = (home_data[col] == unique_value).astype(int)
+def OHE(cat_columns, home_data):
+    new_columns = {}
+    for col in cat_columns:
+        unique_elements = home_data[col].unique()
+        for unique_value in unique_elements:
+            new_columns[f"{col}_{unique_value}"] = (home_data[col] == unique_value).astype(int)
 
-new_columns = pd.DataFrame(new_columns)
-home_data.drop(cat_columns, axis=1, inplace=True)
-home_data = pd.concat([home_data, new_columns], axis=1)
+    new_columns = pd.DataFrame(new_columns)
+    home_data.drop(cat_columns, axis=1, inplace=True)
+    home_data = pd.concat([home_data, new_columns], axis=1)
+
+
+print(home_data)
+print(len(cat_columns))
+Type = input("Enter 0 for Ordinal Encoding and 1 for One Hot Encoding: ")
+if Type == '0':
+    OE(cat_columns, home_data)
+    print(home_data.head())
+elif Type == '1':
+    OHE(cat_columns, home_data)
+    print(home_data.tail())
+
+
+
 
 
 
