35c016bbc9eb4ef92be48367236861672dea9276
Shourya Gupta <119586614+Fireseeker23@users.noreply.github.com>
2024-10-23 17:03:14 +0530

    Update 3.py
    changed OHE function ( unable to concatanate)

--- 1.py
+++ 1.py
@@ -7,7 +7,6 @@ home_data = pd.read_csv(test_data)
 
 
 #Imputer
-
 col_missing = []
 for col in home_data.columns:
     if home_data[col].isnull().any():
@@ -17,7 +16,6 @@ for col in col_missing:
     home_data[col] = home_data[col].fillna(0)
 
 #cat columns
-
 cat_columns = []
 for col in home_data.columns:
     if home_data[col].dtype == 'object':
@@ -38,33 +36,22 @@ def OE(cat_columns, home_data):
             j = j + 1
         i = i + 1
 
-
-
-#One hot encoder
-def OHE(cat_columns, home_data):
-    new_columns = {}
-    for col in cat_columns:
-        unique_elements = home_data[col].unique()
-        for unique_value in unique_elements:
-            new_columns[f"{col}_{unique_value}"] = (home_data[col] == unique_value).astype(int)
-
-    new_columns = pd.DataFrame(new_columns)
-    home_data.drop(cat_columns, axis=1, inplace=True)
-    home_data = pd.concat([home_data, new_columns], axis=1)
-
+    return home_data
 
 print(home_data)
-print(len(cat_columns))
 Type = input("Enter 0 for Ordinal Encoding and 1 for One Hot Encoding: ")
 if Type == '0':
     OE(cat_columns, home_data)
     print(home_data.head())
 elif Type == '1':
-    OHE(cat_columns, home_data)
+    
+    #One hot encoder
+    new_columns = {}
+    for col in cat_columns:
+        unique_elements = home_data[col].unique()
+        for unique_value in unique_elements:
+            new_columns[f"{col}_{unique_value}"] = (home_data[col] == unique_value).astype(int)
+    new_columns = pd.DataFrame(new_columns)
+    home_data.drop(cat_columns, axis=1, inplace=True)
+    home_data = pd.concat([home_data, new_columns], axis=1)
     print(home_data.tail())
-
-
-
-
-
-
